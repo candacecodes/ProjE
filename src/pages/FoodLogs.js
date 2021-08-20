575f4bc72ed365ab3157c39abf97c4968d1c5137
candacecodes <candacehchoe@gmail.com>
2021-08-20 14:19:58 -0700

    almost MVP minus read for logs

--- src/pages/FoodLogs.js
+++ src/pages/FoodLogs.js
@@ -2,6 +2,25 @@ import React, { Component } from "react";
 
 export default class FoodLogs extends Component {
 	render() {
-		return <h1>FoodLogs Page</h1>;
+		return (
+			<>
+				<h1>Food Logs Page</h1>
+				<div>
+					<h3>Log Meal</h3>
+					<br />
+					Date: Today
+					<br />
+					<select name="" id="">
+						<option value="breakfast">Breakfast</option>
+						<option value="lunch">Lunch</option>
+						<option value="dinner">Dinner</option>
+						<option value="snack">Snack</option>
+					</select>
+					<br />
+					Contained which foods?
+					<br />
+				</div>
+			</>
+		);
 	}
 }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -4,6 +4,12 @@ export default class Foods extends Component {
 	state = {
 		food: [],
 		displayFoods: false,
+		displayLogs: false,
+		logFood: [],
+		date: "001",
+		meal: "breakfast",
+		foodsSelected: [],
+		reaction: "neutral",
 	};
 
 	// handle submit for food list
@@ -28,8 +34,22 @@ export default class Foods extends Component {
 		}));
 	};
 
+	//toggle state for add food
+	logFoodToggle = () => {
+		this.setState((prevState) => ({
+			logFood: !prevState.logFood,
+		}));
+	};
+
+	//toggle state for see food logs
+	displayFoodLogToggle = () => {
+		this.setState((prevState) => ({
+			displayLogs: !prevState.displayLogs,
+		}));
+	};
+
+	// delete saved food
 	deleteFood = (item) => {
-		// console.log(item);
 		this.setState({
 			food: this.state.food.filter((e) => e !== item),
 		});
@@ -56,6 +76,138 @@ export default class Foods extends Component {
 		);
 	};
 
+	// log food display function
+	logFood = () => {
+		return (
+			<>
+				<h1>Food Logs Page</h1>
+				<div>
+					<h3>Log Meal</h3>
+					<br />
+					Date <br />
+					<input
+						onChange={(value) => this.handleDateChange(value)}
+						type="date"
+						min="2021-01-01"
+						max="2021-12-31"
+						value="2020-01-01"
+					></input>
+					<br />
+					<br />
+					Select Meal <br />
+					<select
+						defaultValue={this.state.meal}
+						onChange={(value) => this.handleMealChange(value)}
+					>
+						<option value="breakfast">Breakfast</option>
+						<option value="lunch">Lunch</option>
+						<option value="dinner">Dinner</option>
+						<option name="meal" value="snack">
+							Snack
+						</option>
+					</select>
+					<br />
+					<br />
+					Contained which foods?
+					<br />
+					{this.state.food.map((item) => {
+						return (
+							<div>
+								{item}
+								<input
+									value={item}
+									onChange={() => this.updateFoodsSelected(item)}
+									type="checkbox"
+								></input>
+							</div>
+						);
+					})}
+					<br />
+					Food Reaction
+					<br />
+					<select
+						defaultValue={this.state.reaction}
+						onChange={(value) => this.handleReactionChange(value)}
+					>
+						<option value="good">&#128522;</option>
+						<option value="neutral">&#128528;</option>
+						<option value="tired">&#128555;</option>
+						<option value="sleepy">&#128564;</option>
+						<option value="nauseous">&#129314;</option>
+					</select>
+					<br />
+					<br />
+					<button onClick={() => this.submitFoodLog()}>Submit</button>
+				</div>
+			</>
+		);
+	};
+
+	// update foods selected, from radio button
+	updateFoodsSelected = (item) => {
+		if (!this.state.foodsSelected.includes(item)) {
+			this.setState({ foodsSelected: [...this.state.foodsSelected, item] });
+		} else {
+			this.state.foodsSelected.filter((food) => food !== item);
+		}
+		console.log(this.state.foodsSelected);
+	};
+
+	// select meal
+	handleMealChange = (e) => {
+		console.log("update meal");
+		console.log(e.target.value);
+		const updatedMeal = e.target.value;
+		this.setState({ meal: updatedMeal });
+		console.log(this.state);
+	};
+
+	//submit food log
+	submitFoodLog = () => {
+		console.log("submit food log");
+		const date = this.state.date;
+		const meal = this.state.meal;
+		const foodsSelectedSubmission = this.state.foodsSelected;
+		const reaction = this.state.reaction;
+		const newLog = [date, meal, foodsSelectedSubmission, reaction];
+		this.setState({
+			logFood: [...this.state.logFood, newLog],
+		});
+		console.log(this.state);
+	};
+
+	//handle date change
+	handleDateChange = (e) => {
+		console.log("handle date change");
+		const updatedDate = e.target.value;
+		this.setState({ date: updatedDate });
+	};
+
+	//handle reaction change
+	handleReactionChange = (e) => {
+		console.log("update reaction");
+		console.log(e.target.value);
+		const updatedReaction = e.target.value;
+		this.setState({ reaction: updatedReaction });
+		console.log(this.state);
+	};
+
+	// display food logs not working
+	displayFoodLogs = () => {
+		console.log("display food logs");
+		console.log(this.state.logFood);
+		return <div>{this.state.logFood}</div>;
+		// this.state.logFood.map((log) => {
+		// 	return (
+		// 		<>
+		// 			<div>
+		// 				Food Log {log} <button>Delete</button>
+		// 			</div>
+		// 		</>
+		// 	);
+		// });
+	};
+
 	render() {
 		return (
 			<>
@@ -79,6 +231,15 @@ export default class Foods extends Component {
 					<br />
 					{this.state.displayFoods ? this.renderFoods() : null}
 				</div>
+				<div>
+					<button onClick={this.logFoodToggle}>Log a Meal</button>
+					<br />
+					<div>
+						<button onClick={this.displayFoodLogToggle}>See Food Logs</button>
+					</div>
+					{this.state.logFood ? this.logFood() : null}
+				</div>
+				<div>{this.state.displayLogs ? this.displayFoodLogs() : null}</div>
 			</>
 		);
 	}
